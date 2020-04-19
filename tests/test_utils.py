db814b017922db2adc4a005814872b94b727f1e7
Robin Staunton-Collins <robin.staunton-collins@capgemini.com>
2020-04-19 11:59:45 +0200

    Modified utils test to run just once

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,11 +3,8 @@ from bank_api import utils
 
 class TestGenerateRandomAccountNumber():
 
-    account_nums = {}
-
-    @pytest.mark.parametrize('execution_number', range(100))
-    def test_generate_account_number(self, execution_number):
-        """Hardcoded in the number of time this test should be executed"""
+    def test_generate_account_number(self):
         account_num = utils.generate_random_account_number()
         assert len(str(account_num)) == 20
+        assert type(account_num) == int
 
